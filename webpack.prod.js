98b49ae53bb0fda9e6913b72953c67843b65f674
henry-sev <q2466351926@gmail.com>
2021-06-05 23:35:54 +0800

    New plugs

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   mode: "production",
@@ -18,6 +19,8 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
-    })
+    }),
+
+    new BundleAnalyzerPlugin()
   ]
 }
